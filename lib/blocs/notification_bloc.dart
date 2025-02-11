a5c7474a4dbf5c34aa0223a87afb811d93ce9b7a
Marcos Ferreira <marcosferreira@MacBook-Air-de-Marcos.local>
2025-02-11 17:22:31 -0300

    Remove Google Cloud Service Account credentials and use environment variables

--- lib/blocs/notification_bloc.dart
+++ lib/blocs/notification_bloc.dart
@@ -4,13 +4,15 @@ import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart' as http;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import '../config/fcm_config.dart';
 
 class NotificationBloc extends ChangeNotifier {
   Future sendNotification(String title) async {
     final String accessToken = await _getAccessToken();
-    final String projectId = serviceCreds['project_id'];
+    final String projectId =
+        jsonDecode(FcmConfig.serviceAccountCredentials)['project_id'];
 
     var notificationBody = {
       "message": {
@@ -29,7 +31,8 @@ class NotificationBloc extends ChangeNotifier {
 
     try {
       final response = await http.post(
-        Uri.parse('https://fcm.googleapis.com/v1/projects/$projectId/messages:send'),
+        Uri.parse(
+            'https://fcm.googleapis.com/v1/projects/$projectId/messages:send'),
         headers: <String, String>{
           'Content-Type': 'application/json',
           'Authorization': 'Bearer $accessToken',
@@ -47,18 +50,22 @@ class NotificationBloc extends ChangeNotifier {
   }
 
   Future<String> _getAccessToken() async {
-    final accountCredentials = ServiceAccountCredentials.fromJson(serviceCreds);
+    final accountCredentials = ServiceAccountCredentials.fromJson(
+        jsonDecode(FcmConfig.serviceAccountCredentials));
 
     final scopes = ['https://www.googleapis.com/auth/firebase.messaging'];
 
-    final authClient = await clientViaServiceAccount(accountCredentials, scopes);
+    final authClient =
+        await clientViaServiceAccount(accountCredentials, scopes);
 
     final credentials = authClient.credentials;
     return credentials.accessToken.data;
   }
 
-  Future saveToDatabase(String? timestamp, String title, String description) async {
-    final DocumentReference ref = FirebaseFirestore.instance.collection('notifications').doc(timestamp);
+  Future saveToDatabase(
+      String? timestamp, String title, String description) async {
+    final DocumentReference ref =
+        FirebaseFirestore.instance.collection('notifications').doc(timestamp);
     await ref.set({
       'title': title,
       'description': description,
