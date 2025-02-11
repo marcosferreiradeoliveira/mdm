ade8cbee3007c398b98e72ca0e67d15a07d9a8ba
Marcos Ferreira <marcosferreira@MacBook-Air-de-Marcos.local>
2025-02-11 17:14:19 -0300

    Move Firebase configuration to .env

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,23 @@ import 'blocs/notification_bloc.dart';
 import 'blocs/comment_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main()async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: FirebaseOptions(
+      apiKey: dotenv.env['FIREBASE_API_KEY']!,
+      authDomain: dotenv.env['FIREBASE_AUTH_DOMAIN']!,
+      projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
+      messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+      appId: dotenv.env['FIREBASE_APP_ID']!,
+      measurementId: dotenv.env['FIREBASE_MEASUREMENT_ID'],
+    ),
   );
   runApp(MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -23,42 +33,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-      ChangeNotifierProvider<AdminBloc>(create: (context) => AdminBloc()),
-      ChangeNotifierProvider<CommentBloc>(create: (context) => CommentBloc()),
-      ChangeNotifierProvider<NotificationBloc>(create: (context) => NotificationBloc())
-
-    ],
-    child: MaterialApp(
-      debugShowCheckedModeBanner: false,
-      scrollBehavior: TouchAndMouseScrollBehavior(),
-      theme: ThemeData(
-        useMaterial3: false,
-        scaffoldBackgroundColor: Colors.white,
-        textTheme: GoogleFonts.poppinsTextTheme(),
-        appBarTheme: AppBarTheme(
-          color: Colors.white,
-          titleTextStyle: GoogleFonts.poppins(
-            color: Colors.grey[900],fontWeight: FontWeight.w600, fontSize: 18
-          ),
-          elevation: 0,
-          
-          actionsIconTheme: IconThemeData(
-            color: Colors.grey[900],
-          ),
-          iconTheme: IconThemeData(
-            color: Colors.grey[900]
-          )
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AdminBloc>(create: (context) => AdminBloc()),
+        ChangeNotifierProvider<CommentBloc>(create: (context) => CommentBloc()),
+        ChangeNotifierProvider<NotificationBloc>(
+            create: (context) => NotificationBloc())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        scrollBehavior: TouchAndMouseScrollBehavior(),
+        theme: ThemeData(
+          useMaterial3: false,
+          scaffoldBackgroundColor: Colors.white,
+          textTheme: GoogleFonts.poppinsTextTheme(),
+          appBarTheme: AppBarTheme(
+              color: Colors.white,
+              titleTextStyle: GoogleFonts.poppins(
+                  color: Colors.grey[900],
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18),
+              elevation: 0,
+              actionsIconTheme: IconThemeData(
+                color: Colors.grey[900],
+              ),
+              iconTheme: IconThemeData(color: Colors.grey[900])),
         ),
-        
+        home: MyApp1(),
       ),
-      home: MyApp1(),
-    ),
-    
-    
-    
-    
-    
     );
   }
 }
@@ -73,13 +75,12 @@ class MyApp1 extends StatelessWidget {
   }
 }
 
-
 class TouchAndMouseScrollBehavior extends MaterialScrollBehavior {
-    // Override behavior methods and getters like dragDevices
-    @override
-    Set<PointerDeviceKind> get dragDevices => { 
-      PointerDeviceKind.touch,
-      PointerDeviceKind.mouse,
-      // etc.
-    };
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        // etc.
+      };
 }
